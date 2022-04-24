062686ed3419c89d27b51e4c5bb273ee1c4a0d98
sokim03 <sokim03@gmail.com>
2022-04-24 13:25:43 -0700

    adding updated schema

--- challenge_schema.sql
+++ challenge_schema.sql
@@ -1,26 +1,14 @@
 -- Create Active User Table
 CREATE TABLE customers_table (
- id INT PRIMARY KEY NOT NULL,
- first_name TEXT,
- last_name TEXT,
- username TEXT
 );
 
 CREATE TABLE products_table (
- billing_id INT PRIMARY KEY NOT NULL,
- street_address TEXT,
- state TEXT,
- username TEXT
 );
 
 CREATE TABLE review_id_table (
- billing_id INT PRIMARY KEY NOT NULL,
- cc_encrypted TEXT
 );
 
 CREATE TABLE vine_table (
- billing_id INT PRIMARY KEY NOT NULL,
- cc_encrypted TEXT
 );
 
 
